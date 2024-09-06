749b0ab5063bdaf3481d9ae87c14988eec47289a
JenChieh <jcs090218@gmail.com>
2024-09-06 16:42:55 -0700

    fix: build

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  # List of your packages!
+  flx-rs = pkgs.callPackage ./pkgs/flx-rs { };
 }
