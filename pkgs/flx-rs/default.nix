23cd9c7927273e0d5b3fc4d9db47b71e172f2ddb
JenChieh <jcs090218@gmail.com>
2024-09-06 16:42:50 -0700

    fix indentation

--- pkgs/flx-rs/default.nix
+++ pkgs/flx-rs/default.nix
@@ -1,29 +1,29 @@
 {
-  rustPlatform,
-  fetchFromGitHub,
-  lib,
+	rustPlatform,
+	fetchFromGitHub,
+	lib,
 }:
 
 rustPlatform.buildRustPackage rec {
-  pname = "flx-rs";
-  version = "0.2.0";
+	pname = "flx-rs";
+	version = "0.2.0";
 
-  src = fetchFromGitHub {
-    owner = "jcs090218";
-    repo = pname;
-    rev = "${version}";
-    sha256 = "sha256-OFm0Jk06Mxzr4F7KrMBGFqcDSuTtrMvBSK99bbOgua4=";
-  };
+	src = fetchFromGitHub {
+		owner = "jcs090218";
+		repo = pname;
+		rev = "${version}";
+		sha256 = "sha256-OFm0Jk06Mxzr4F7KrMBGFqcDSuTtrMvBSK99bbOgua4=";
+	};
 
-  cargoHash = "sha256-JMRcXuw6AKfwViOgYAgFdSwUeTo04rEkKj+t+W8wjGI=";
-  doCheck = false;
+	cargoHash = "sha256-JMRcXuw6AKfwViOgYAgFdSwUeTo04rEkKj+t+W8wjGI=";
+	doCheck = false;
 
-  meta = {
-    description = "Rewrite emacs-flx in Rust for dynamic modules";
-    homepage = "https://github.com/jcs090218/flx-rs";
-    license = lib.licenses.mit;
-	maintainers = [ 
-	  jcs090218
-	];
-  };
+	meta = {
+		description = "Rewrite emacs-flx in Rust for dynamic modules";
+		homepage = "https://github.com/jcs090218/flx-rs";
+		license = lib.licenses.mit;
+		maintainers = [
+			jcs090218
+		];
+	};
 }
