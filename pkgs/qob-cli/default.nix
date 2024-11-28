ddcef709ceaeda79a1bdb08c6059f4def1036292
JenChieh <jcs090218@gmail.com>
2024-11-28 04:19:16 -0800

    feat: Try build nix

--- pkgs/qob-cli/default.nix
+++ pkgs/qob-cli/default.nix
@@ -19,8 +19,7 @@ stdenv.mkDerivation rec {
   buildInputs = [ sbcl ];
 
   installPhase = ''
-    make install-ql
-    make build
+    make build-nix
     install -m755 -D bin/sbcl/qob $out/bin/qob
   '';
 
